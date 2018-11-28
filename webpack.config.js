6fb4a693fad208b988b03dc7d812e605c3a2b02f
dashayaskova <dashayaskova111@gmail.com>
2018-11-28 07:48:22 +0300

    додали sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +7,32 @@ module.exports = {
         path: path.resolve(__dirname, 'docs'),
         filename: 'js/main.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            // ...additional rules...
+        ],
+        rules: [
+            // ...additional rules...
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+
+    },
+
 };
